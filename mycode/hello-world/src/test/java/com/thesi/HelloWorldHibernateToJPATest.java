524147e0f1206ccdfe081bc83e43e1a59b3e997a
caithesi <caithesi@gmail.com>
2025-09-09 16:33:29 +0700

    hibernate to jpa test

---
+++ mycode/hello-world/src/test/java/com/thesi/HelloWorldHibernateToJPATest.java
@@ -0,0 +1,68 @@
+package com.thesi;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.hibernate.cfg.Configuration;
+import org.junit.jupiter.api.Test;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldHibernateToJPATest {
+    private static EntityManagerFactory createEntityManagerFactory() {
+        Configuration configuration = new Configuration();
+        // this doesnt need due to hibernate-scan-jandex
+//        configuration.configure().addAnnotatedClass(Message.class);
+
+        Map<String, String> properties = new HashMap<>();
+        Enumeration<?> propertyNames = configuration.getProperties().propertyNames();
+        while (propertyNames.hasMoreElements()) {
+            String element = (String) propertyNames.nextElement();
+            properties.put(element, configuration.getProperties().getProperty(element));
+        }
+
+        return Persistence.createEntityManagerFactory("ch02", properties);
+    }
+
+    @Test
+    public void storeLoadMessage() {
+
+        try (EntityManagerFactory emf = createEntityManagerFactory();
+             EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+
+            Message message = new Message();
+            message.setText("Hello World!");
+
+            em.persist(message);
+
+            em.getTransaction().commit();
+            //INSERT into MESSAGE (ID, TEXT) values (1, 'Hello World!')
+
+            em.getTransaction().begin();
+
+            List<Message> messages =
+                    em.createQuery("select m from Message m", Message.class).getResultList();
+            //SELECT * from MESSAGE
+
+            messages.get(messages.size() - 1).setText("Hello World from JPA!");
+
+            em.getTransaction().commit();
+            //UPDATE MESSAGE set TEXT = 'Hello World from JPA!' where ID = 1
+
+            assertAll(
+                    () -> assertEquals(1, messages.size()),
+                    () -> assertEquals("Hello World from JPA!", messages.get(0).getText())
+            );
+
+            em.close();
+
+        }
+    }
+}
